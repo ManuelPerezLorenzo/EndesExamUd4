523debcf73a060fe7f8fa3e20c4f2d3cd1f69115
Manuel Perez <mperlor3007@g.educaand.es>
2026-06-18 11:23:31 +0200

    Version con JavaDoc

---
+++ JavaDoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"Model","l":"Circulo"},{"p":"Model","l":"Figura"},{"p":"Service","l":"GestorFiguras"},{"p":"<Unnamed>","l":"Main"},{"p":"Model","l":"Rectangulo"},{"p":"Model","l":"Triangulo"}];updateSearchResults();
